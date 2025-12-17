443723c0272d94af52b260cd64c22290133243da
bagus <mbagusrd@gmail.com>
2025-12-17 19:00:14 +0700

    Add Livewire update and script route configuration in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
+
+Livewire::setUpdateRoute(function ($handle) {
+    return Route::post(env('PUBLIC_PATH') . '/livewire/update', $handle);
+});
+
+Livewire::setScriptRoute(function ($handle) {
+    return Route::get(env('APP_URL') . '/livewire/livewire.js', $handle);
+});
 
 Route::get('/', function () {
     return view('welcome');
