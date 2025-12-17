b37bcdd91adf7a4a61d666e3d9683fad8bdecc7d
bagus <mbagusrd@gmail.com>
2025-12-17 20:10:34 +0700

    add UserSeeder to initialize admin user and role

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('1'),
+        ]);
+
+        Role::create(['name' => 'admin']);
+
+        $user->assignRole('admin');
+    }
+}
